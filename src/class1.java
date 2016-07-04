d48582bfa51f724a7c6485f310cfa060dd111569
Razvan Rosu <razvan1theo@yahoo.com>
2016-07-04 14:32:32 +0300

    Revert "test1"
    This reverts commit b9167cb1abc63d6835b5615b5196a44a9e7d7f31.

--- src/class1.java
+++ src/class1.java
@@ -3,8 +3,7 @@ public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ABCDEf");
-
+		System.out.println("abdasfdf");
 	}
 
 }
