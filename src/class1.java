89650fe33ac805fa7fc7858e5bac048b76e85956
Razvan Rosu <razvan1theo@yahoo.com>
2016-07-04 14:33:34 +0300

    pp

--- src/class1.java
+++ src/class1.java
@@ -3,7 +3,8 @@ public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abdasfdf");
+		System.out.println("ABCDEf");
+
 	}
 
 }
